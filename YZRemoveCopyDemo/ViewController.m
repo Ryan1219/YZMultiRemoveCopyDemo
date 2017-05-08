16e380560787899f9c2f9badd9d3c17e85021b72
Ryan1219 <mrzhang1219@163.com>
2017-05-08 13:15:38 +0800

    去除字符串中不相连的相同字符  去掉一串字符串的重复字符，只是保留一个

--- YZRemoveCopyDemo/ViewController.m
+++ YZRemoveCopyDemo/ViewController.m
@@ -16,14 +16,118 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self removeCopy];
+    [self removeMultiCopy];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//MARK:- 去掉一串字符串的重复字符，只是保留一个
+- (void)removeMultiCopy {
+    
+    NSString *str = @"aabdffccabdeacg";
+    NSMutableString *newString = [[NSMutableString alloc] init];
+    for (int i = 0; i < str.length; i++) {
+        unsigned char a = [str characterAtIndex:i];
+        if ([newString containsString:[NSString stringWithFormat:@"%c",a]] == false) {
+            [newString appendString:[NSString stringWithFormat:@"%c",a]];
+        } else {
+            NSLog(@"--had containted--");
+        }
+    }
+    NSLog(@"----%@",newString);
+   
 }
 
+//MARK:- 去除字符串中不相连的重复字符，如aabcad 去除后为 aabcd
+- (void)removeCopy {
+    
+    NSMutableString *str = [[NSMutableString alloc] initWithFormat:@"aabcca"];
+    for (int i = 0; i < str.length-1; i++) {
+        for (int j = i + 1; j < str.length; j++) {
+            
+            unsigned char a = [str characterAtIndex:i];
+            unsigned char b = [str characterAtIndex:j];
+            
+            if (a == b) {
+                if (j - i > 1) {
+                    NSRange range = {j,1};
+                    [str deleteCharactersInRange:range];
+                    j = i--;
+                }
+            }
+        }
+    }
+    NSLog(@"----%@",str);
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
